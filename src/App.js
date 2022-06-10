2cc3d023a8bc190849acf4d0e27e7a5ac5aa5097
Anastasiya999 <anapechko@gmail.com>
2022-06-10 15:33:04 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./styles/App.scss";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center">
+          <img width={40} height={40} src="/img/logo.png"></img>
+          <div>
+            <h3 className="text-uppercase">React sneakers</h3>
+            <p>Shop with the best sneakers</p>
+          </div>
+        </div>
+
+        <ul className="d-flex">
+          <li className="mr-30">
+            <img width={18} height={18} src="/img/cart.svg"></img>
+            <span>100</span>
+          </li>
+          <li>
+            <img width={18} height={18} src="/img/user.svg"></img>
+          </li>
+        </ul>
       </header>
+      <div className="content p-40">
+        <h1 className="mb-40">All sneakers</h1>
+
+        <div className="card">
+          <img width={133} height={112} src="/img/sneakers/1.jpg" />
+          <h5>Nike Blaser Mid Suede</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Price</span>
+              <b>120 pln</b>
+            </div>
+            <button>
+              <img src="/img/plus.svg" height={11} width={11} />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
