0f8f61d514a9ad16acb7b971b1a5ac8d704d84cb
Anastasiya999 <anapechko@gmail.com>
2022-06-11 08:33:46 +0200

    added mockapi with items

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./styles/App.scss";
 import Card from "./components/Card";
 import Header from "./components/Header";
@@ -9,13 +10,70 @@ const sneakers = [
     price: 100,
     imageUrl: "/img/sneakers/1.jpg",
   },
+  {
+    title: "Nike Air Max 270",
+    price: 120,
+    imageUrl: "/img/sneakers/2.jpg",
+  },
+  {
+    title: "Nike Blazer Mid Suede",
+    price: 400,
+    imageUrl: "/img/sneakers/3.jpg",
+  },
+  {
+    title: "Aka Boku Future Rider",
+    price: 500,
+    imageUrl: "/img/sneakers/4.jpg",
+  },
+  {
+    title: "Under Armour Curry 8",
+    price: 320,
+    imageUrl: "/img/sneakers/5.jpg",
+  },
+  {
+    title: "Nike Kyrie 7",
+    price: 240,
+    imageUrl: "/img/sneakers/6.jpg",
+  },
+  {
+    title: "Jordan Air Jordan 11",
+    price: 150,
+    imageUrl: "/img/sneakers/7.jpg",
+  },
+  {
+    title: "Nike LeBron XVIII",
+    price: 420,
+    imageUrl: "/img/sneakers/8.jpg",
+  },
+  {
+    title: "Nike Lebron XVIII Low",
+    price: 510,
+    imageUrl: "/img/sneakers/9.jpg",
+  },
+  {
+    title: "Nike Kyrie Flytrap IV",
+    price: 310,
+    imageUrl: "/img/sneakers/10.jpg",
+  },
 ];
 function App() {
+  const [cartOpened, setCartOpened] = React.useState(false);
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://62a42f2447e6e400638da88e.mockapi.io/items")
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setItems(json);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
-      <Drawer />
-
-      <Header />
+      {cartOpened && <Drawer onClose={() => setCartOpened(false)} />}
+      <Header onClickCart={() => setCartOpened(true)} />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
           <h1>All sneakers</h1>
@@ -24,8 +82,8 @@ function App() {
             <input placeholder="Type to search..." />
           </div>
         </div>
-        <div className="d-flex">
-          {sneakers.map((item, index) => {
+        <div className="d-flex flex-wrap">
+          {items.map((item, index) => {
             return (
               <Card
                 key={index}
