56b56498f135bf161b0fac0453aaaebb311c4346
Anastasiya999 <anapechko@gmail.com>
2022-06-14 21:01:09 +0200

    changed subheader style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,8 +29,8 @@ function Home({
   }
   return (
     <div className="content p-40">
-      <div className="d-flex align-center flex-wrap justify-between mb-40">
-        <h1>All sneakers</h1>
+      <div className="subheader d-flex align-center flex-wrap justify-between mb-40 pb-10">
+        <h1>All plants</h1>
         <div className="search-block d-flex">
           <img src="/img/search.svg" alt="Search" />
           {searchValue && (
